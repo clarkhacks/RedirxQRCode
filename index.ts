8e1152860890f2375981043f45a4def78dbe5318
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2024-08-15 16:15:03 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,42 @@
+const qr = require('qr-image')
+
+export default {
+  async fetch(
+    req: Request,
+    env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    const url = new URL(req.url)
+    const text = url.searchParams.get('url') || 'https://my.redirx.top'
+    const referer = req.headers.get('Referer') || ''
+
+    const allowedDomains = ['redirx.top', 'clark.today', 'weckmann.me']
+    const isAllowed = allowedDomains.some(domain => referer.includes(domain))
+
+    if (!isAllowed) {
+      return new Response('Forbidden', { status: 403 })
+    }
+
+    if (req.method === 'GET') {
+      return generateQRCode(text)
+    }
+
+    return new Response('Method not allowed', { status: 405 })
+  },
+}
+
+async function generateQRCode(text: string): Promise<Response> {
+  const headers = {
+    'Content-Type': 'image/png',
+    'Cache-Control': 'public, max-age=31536000', // 1 year cache
+    'Content-Disposition': `inline; filename="qr-${text
+      .replace(/^https?:\/\//, '')
+      .replace(/[^a-z0-9]/gi, '_')}.png"`,
+  }
+  const options = {
+    type: 'png',
+    margin: 1,
+  }
+  const qr_png = qr.imageSync(text, options)
+  return new Response(qr_png, { headers })
+}
